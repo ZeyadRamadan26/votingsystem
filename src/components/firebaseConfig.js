3ce8311d74269b9f338452ab7b0f8e021966fc19
Zeyad Ramadan <93614653+ZeyadRamadan26@users.noreply.github.com>
2024-05-14 23:14:55 +0300

    Update firebaseConfig.js

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -2,12 +2,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: "hci-project-bb492.firebaseapp.com",
-  projectId: "hci-project-bb492",
-  storageBucket: "hci-project-bb492.appspot.com",
-  messagingSenderId: "205663049606",
-  appId: "1:205663049606:web:c6e54c66fb2454b2e93e07"
 };
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
